cfaf0505d588c353b0b34ce3a95f04cba8ad233c
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-07-31 22:34:35 -0300

    Create params base routes

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,8 @@ routes.get("/", (req, res) => res.render("index"))
 routes.get("/room", (req, res) => res.render("room"))
 routes.get("/create-pass", (req, res) => res.render("create-pass"))
 
+// formato que o formulario de dentro da modal tem que passar a informacao
+//(req, res) => res.render("exemplo", {req})
+routes.get('/room/:room/:question/:action')
+
 module.exports = routes
